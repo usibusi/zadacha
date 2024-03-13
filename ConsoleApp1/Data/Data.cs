fad1e18664472516ff238aafd203199a2fe9c2c3
usibusi <daskalo05@abv.bg>
2024-03-13 17:18:27 +0200

    proektche

---
+++ ConsoleApp1/Data/Data.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Data
+{
+    public class Data
+    {
+        public List<Product> GetAll()
+        {
+            var productList = new List<Product>();
+            SqlConnection con = Database.GetConnection();
+            con.Open();
+            using (con)
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = con;
+                command.CommandText = "Select * From product";
+                SqlDataReader reader = command.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Product product = new Product((int)reader.GetValue(0), (string)reader.GetValue(1), (decimal)reader.GetValue(2), (int)reader.GetValue(3));
+                        productList.Add(product);
+                    }
+                }
+            }
+            con.Close();
+            return productList;
+        }
+    }
+}
---
+++ ConsoleApp1/Data/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Data
+{
+    public class Database
+    {
+        private string connectionString = "Data source = DESKTOP-M834BAO; Database = az; integrated security = true";
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
